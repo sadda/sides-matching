8bfedc7876e71ebdc6801da01d401c59cf43c8c5
sadda <lukas.adam.cr@gmail.com>
2024-04-30 10:23:28 +0200

    Added splits of the similarity matrix

--- analysis.py
+++ analysis.py
@@ -3,8 +3,11 @@ import numpy as np
 import pandas as pd
 
 class Analysis():
-    def __init__(self, *args, **kwargs):
-        raise NotImplementedError('Must be implemented by subclasses')
+    def __init__(self):
+        # TODO: change names to something normal
+        self.n_sides = len(self.sides)
+        self.names1 = ['database-database', 'query-query', 'database-query']
+        # TODO: add check for sides
 
     def get_split(self, *args, **kwargs):
         raise NotImplementedError('Must be implemented by subclasses')
@@ -39,10 +42,53 @@ class Analysis():
         matches.index = 'match k = ' + (matches.index + 1).astype(str)
         return matches
 
+    def initialite_results_split_similarity(self):
+        results = {}
+        for name1 in self.names1:
+            results1 = {}
+            for diff in range(self.n_sides):
+                results2 = {}
+                for name2 in self.names2:
+                    results2[name2] = []
+                results1[diff] = results2
+            results[name1] = results1
+        return results
+
+    def compute_data_split_similarity(self, df):
+        identity = df['identity'].to_numpy()
+        orientation = df['orientation'].apply(lambda x: self.sides[x] if x in self.sides else np.nan).to_numpy()
+        return {'identity': identity, 'orientation': orientation}
+    
+    def compute_index_split_similarity(self, data, i, j):
+        if data['identity'][i] == data['identity'][j]:
+            return 'same ind'
+        else:
+            return 'diff ind'
+
+    def split_similarity_matrix(self, df, similarity, idx_database, idx_query):
+        if len(set(idx_database).intersection(set(idx_query))):
+            raise Exception('idx must be disjoint')
+        idx_database = np.sort(np.array(idx_database))
+        idx_query = np.sort(np.array(idx_query))
+        
+        data = self.compute_data_split_similarity(df)
+        results = self.initialite_results_split_similarity()
+        for idx1, idx2, name in zip((idx_database, idx_query, idx_database), (idx_database, idx_query, idx_query), self.names1):
+            array_equal = np.array_equal(idx1, idx2)
+            for i_index, i in enumerate(idx1):
+                idx2_range = idx2[i_index+1:] if array_equal else idx2
+                for j in idx2_range:
+                    diff = abs(data['orientation'][i] - data['orientation'][j])
+                    index = self.compute_index_split_similarity(data, i, j)
+                    results[name][diff][index].append(similarity[i,j])
+        return results
+
 class Analysis_SarahZelvy(Analysis):
     def __init__(self):
         self.sides = {'left': 0, 'right': 1}
         self.diff_to_matches = {0: 'match same side', 1: 'match diff side'}
+        self.names2 = ['same ind', 'diff ind']
+        super().__init__()
 
     def get_split(self, df, daytime='all'):
         idx_database = []
@@ -102,10 +148,40 @@ class Analysis_SeaTurtleIDHeads(Analysis_WildlifeDataset):
     def __init__(self):
         self.sides = {'left': 0, 'topleft': 1, 'top': 2, 'topright': 3, 'right': 4}
         self.diff_to_matches = {0: 'match diff = 0', 1: 'match diff = 1', 2: 'match diff = 2', 3: 'match diff = 3', 4: 'match diff = 4'}
+        self.names2 = ['same ind - same day', 'same ind - same setup', 'same ind - diff setup', 'diff ind - same setup', 'diff ind - diff setup']
+        super().__init__()
         
+    def get_setup_id(self, date):
+        if date.year <= 2013:
+            return 1
+        elif date.year <= 2017:
+            return 2
+        else:
+            return 3
+
+    def compute_data_split_similarity(self, df):
+        data = super().compute_data_split_similarity(df)
+        data['date'] = pd.to_datetime(df['date']).to_numpy()
+        data['camera_setup'] = pd.to_datetime(df['date']).apply(lambda x: self.get_setup_id(x)).to_numpy()
+        return data
+    
+    def compute_index_split_similarity(self, data, i, j):
+        if data['identity'][i] == data['identity'][j]:
+            if data['date'][i] == data['date'][j]:
+                return 'same ind - same day'
+            elif data['camera_setup'][i] == data['camera_setup'][j]:
+                return 'same ind - same setup'
+            else:
+                return 'same ind - diff setup'
+        else:
+            if data['camera_setup'][i] == data['camera_setup'][j]:
+                return 'diff ind - same setup'
+            else:
+                return 'diff ind - diff setup'
+
 class Analysis_ZindiTurtleRecall(Analysis_WildlifeDataset):
     def __init__(self):
         self.sides = {'left': 0, 'top': 1, 'right': 2}
         self.diff_to_matches = {0: 'match diff = 0', 1: 'match diff = 1', 2: 'match diff = 2'}
-
-
+        self.names2 = ['same ind', 'diff ind']
+        super().__init__()
