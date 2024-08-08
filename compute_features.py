e35c355c8607039c5ae38e5f0d627bbe964eb4af
sadda <lukas.adam.cr@gmail.com>
2024-08-08 06:44:45 +0000

    Clean up

--- compute_features.py
+++ compute_features.py
@@ -1,24 +1,14 @@
 import os
 import torchvision.transforms as T
-
-from wildlife_datasets import datasets
 from utils import AmvrakikosTurtles, ReunionTurtles, SeaTurtleIDSubset, WD, get_extractor, get_normalized_features
 
 model_name = 'MegaDescriptor-L-384'
 root_datasets = '/data/wildlife_datasets/data'
 
 dataset_classes = [
-    (datasets.HyenaID2022, 'HyenaID2022'),
     (AmvrakikosTurtles, 'Amvrakikos Turtles'),
     (ReunionTurtles, 'ReunionTurtles'),
     (SeaTurtleIDSubset, 'SeaTurtleIDSubset'),
-    (datasets.LeopardID2022, 'LeopardID2022'),
-    (datasets.NyalaData, 'NyalaData'),    
-    (datasets.SeaTurtleID2022, 'SeaTurtleID2022'),
-    (datasets.SeaTurtleIDHeads, 'SeaTurtleIDHeads'),
-    (datasets.StripeSpotter, 'StripeSpotter'),
-    (datasets.WhaleSharkID, 'WhaleSharkID'),
-    (datasets.ZindiTurtleRecall, 'ZindiTurtleRecall'),
 ]
 
 img_size = int(model_name.split('-')[-1])
