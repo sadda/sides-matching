a3f2baddb280f21d40b22bcfc29aac3b8d0dbec6
sadda <lukas.adam.cr@gmail.com>
2024-08-27 06:05:19 +0000

    Export boxplot for tikz

--- utils.py
+++ utils.py
@@ -298,3 +298,15 @@ def get_transform(flip=False, grayscale=False, img_size=None, normalize=False):
     if normalize:
         transform = T.Compose([*transform.transforms, T.ToTensor(), T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))])
     return transform
+
+def get_box_plot_data(boxplot):
+    rows_list = []
+    for i in range(len(boxplot['boxes'])):
+        dict1 = {}
+        dict1['lower_whisker'] = boxplot['whiskers'][i*2].get_ydata()[1]
+        dict1['lower_quartile'] = boxplot['boxes'][i].get_ydata()[1]
+        dict1['median'] = boxplot['medians'][i].get_ydata()[1]
+        dict1['upper_quartile'] = boxplot['boxes'][i].get_ydata()[2]
+        dict1['upper_whisker'] = boxplot['whiskers'][(i*2)+1].get_ydata()[1]
+        rows_list.append(dict1)
+    return pd.DataFrame(rows_list)
