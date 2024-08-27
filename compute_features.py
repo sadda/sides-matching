0915cfbdc62d994abdaf346d29e77c49acc5e1a2
sadda <lukas.adam.cr@gmail.com>
2024-08-27 06:06:19 +0000

    Added grayscale to compute_features

--- compute_features.py
+++ compute_features.py
@@ -1,6 +1,5 @@
 import os
-import torchvision.transforms as T
-from utils import AmvrakikosTurtles, ReunionTurtles, SeaTurtleIDSubset, WD, get_extractor, get_normalized_features
+from utils import AmvrakikosTurtles, ReunionTurtles, SeaTurtleIDSubset, WD, get_extractor, get_normalized_features, get_transform
 
 model_name = 'MegaDescriptor-L-384'
 root_datasets = '/data/wildlife_datasets/data'
@@ -14,18 +13,15 @@ dataset_classes = [
 img_size = int(model_name.split('-')[-1])
 for dataset_class, dataset_root in dataset_classes:
     for flip in [True, False]:
-        if flip:
-            transform = T.Compose([T.RandomHorizontalFlip(1), T.Resize([img_size, img_size]), T.ToTensor(), T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))])
-        else:
-            transform = T.Compose([T.Resize([img_size, img_size]), T.ToTensor(), T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))])
-
-        root = os.path.join(root_datasets, dataset_root)
-        file_name = os.path.join('features', f'features_{dataset_class.__name__}_flip={flip}_{model_name}.npy')
-        if not os.path.exists(file_name):
-            d = dataset_class(root)
-            if 'bbox' in d.df:
-                dataset = WD(d.df, d.root, transform=transform, img_load='bbox')
-            else:
-                dataset = WD(d.df, d.root, transform=transform)
-            extractor = get_extractor(model_name='hf-hub:BVRA/'+model_name, batch_size=32, device='cuda')
-            features = get_normalized_features(file_name, dataset, extractor)
+        for grayscale in [True, False]:
+            transform = get_transform(flip=flip, grayscale=grayscale, img_size=img_size, normalize=True)
+            root = os.path.join(root_datasets, dataset_root)
+            file_name = os.path.join('features', f'features_{dataset_class.__name__}_flip={flip}_grayscale={grayscale}_{model_name}.npy')
+            if not os.path.exists(file_name):
+                d = dataset_class(root)
+                if 'bbox' in d.df:
+                    dataset = WD(d.df, d.root, transform=transform, img_load='bbox')
+                else:
+                    dataset = WD(d.df, d.root, transform=transform)
+                extractor = get_extractor(model_name='hf-hub:BVRA/'+model_name, batch_size=32, device='cuda')
+                features = get_normalized_features(file_name, dataset, extractor)
