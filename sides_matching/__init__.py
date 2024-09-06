cce08a4658e2ca370fede5f81376137a39d9e1ab
sadda <lukas.adam.cr@gmail.com>
2024-09-06 08:17:46 +0000

    Refactor

--- sides_matching/__init__.py
+++ sides_matching/__init__.py
@@ -1,7 +1,7 @@
 from .analysis import Analysis, Analysis_WildlifeDataset
 from .analysis import Analysis_HyenaID2022, Analysis_LeopardID2022, Analysis_NyalaData, Analysis_SeaTurtleID2022, Analysis_SeaTurtleIDHeads, Analysis_StripeSpotter, Analysis_WhaleSharkID, Analysis_ZindiTurtleRecall
 from .analysis import Analysis_AmvrakikosTurtles, Analysis_ReunionTurtles, Analysis_ZakynthosTurtles
+from .datasets import WD, AmvrakikosTurtles, ReunionTurtles, ZakynthosTurtles
 from .predictions import Data_MegaDescriptor, Data_TORSOOI, Data_SIFT, Prediction
-from .utils import AmvrakikosTurtles, ReunionTurtles, ZakynthosTurtles
 from .utils import get_normalized_features, get_extractor, compute_predictions, compute_predictions_disjoint, compute_predictions_closed
-from .utils import WD, get_dataset, get_df_split, unique_no_sort, get_transform, get_box_plot_data
+from .utils import get_dataset, get_df_split, unique_no_sort, get_transform, get_box_plot_data
--- sides_matching/utils.py
+++ sides_matching/utils.py
@@ -1,90 +1,12 @@
 import os
-import copy
 import numpy as np
 import pandas as pd
 import timm
 from sklearn.metrics.pairwise import cosine_similarity
 import torchvision.transforms as T
-from wildlife_datasets import datasets
 from wildlife_tools.data import WildlifeDataset
 from wildlife_tools.features import DeepFeatures
 from typing import Optional, List, Tuple, Callable
-
-class AmvrakikosTurtles(datasets.DatasetFactory):
-    def create_catalogue(self) -> pd.DataFrame:
-        data = pd.read_csv(os.path.join(self.root, 'annotations.csv'))
-
-        # Get the bounding box
-        columns_bbox = ['bbox_x', 'bbox_y', 'bbox_width', 'bbox_height']
-        bbox = data[columns_bbox].to_numpy()
-        bbox = pd.Series(list(bbox))
-
-        # Finalize the dataframe
-        df = pd.DataFrame({
-            'image_id': range(len(data)),
-            'path': 'images' + os.path.sep + data['image_name'],
-            'identity': data['image_name'].apply(lambda x: x.split('_')[0]).astype(int),
-            'date': data['image_name'].apply(lambda x: x.split('_')[1]).astype(int),
-            'orientation': data['image_name'].apply(lambda x: x.split('_')[2]),
-            'bbox': bbox,
-        })
-        df = df[df['orientation'] != 'top']
-        df['image_id'] = range(len(df))
-        return self.finalize_catalogue(df)
-
-class ReunionTurtles(datasets.DatasetFactory):
-    def create_catalogue(self) -> pd.DataFrame:
-        data = pd.read_csv(os.path.join(self.root, 'data.csv'))
-
-        date = pd.to_datetime(data['Date'])
-        year = date.apply(lambda x: x.year)
-        path = data['Species'] + os.path.sep + data['Turtle_ID'] + os.path.sep + year.astype(str) + os.path.sep + data['Photo_name']
-        orientation = data['Photo_name'].apply(lambda x: os.path.splitext(x)[0].split('_')[2])
-        orientation = orientation.replace({'L': 'left', 'R': 'right'})
-
-        # Extract and convert ID codes
-        id_code = list(data['ID_Code'].apply(lambda x: x.split(';')))
-        max0 = 0
-        max1 = 0
-        for x in id_code:
-            for y in x:
-                max0 = max(max0, int(y[0]))
-                max1 = max(max1, int(y[1]))
-        code = np.zeros((len(id_code), max0, max1), dtype=int)
-        for i, x in enumerate(id_code):
-            for y in x:
-                code[i, int(y[0])-1, int(y[1])-1] = int(y[2])
-        code = code.reshape(len(id_code), -1)
-
-        # Finalize the dataframe
-        df = pd.DataFrame({
-            'image_id': range(len(data)),
-            'path': path,
-            'identity': data['Turtle_ID'],
-            'date': date,
-            'orientation': orientation,
-            'species': data['Species'],
-            'id_code': list(code)
-        })
-        return self.finalize_catalogue(df)
-
-class ZakynthosTurtles(datasets.DatasetFactory):
-    def create_catalogue(self) -> pd.DataFrame:
-        data = pd.read_csv(os.path.join(self.root, 'annotations.csv'))
-        bbox = pd.read_csv(os.path.join(self.root, 'bbox.csv'))
-        data = pd.merge(data, bbox, left_on='path', right_on='image_name')
-
-        dates = data['date'].str.split('_')
-        dates = dates.apply(lambda x: x[2] + '-' + x[1] + '-' + x[0])
-        df = pd.DataFrame({
-            'image_id': range(len(data)),
-            'path': 'images/' + data['path'],
-            'identity': data['identity'],
-            'date': dates,
-            'orientation': data['orientation'],
-            'bbox': data[['bbox_x', 'bbox_y', 'bbox_width', 'bbox_height']].values.tolist()
-        })
-        return self.finalize_catalogue(df)
 
 def get_normalized_features(
         file_name: str,
@@ -232,33 +154,6 @@ def compute_predictions_closed(
     """
 
     return compute_predictions(features_query, features_database, ignore=ignore, **kwargs)
-
-class WD(WildlifeDataset):
-    def plot_grid(self, transform=None, **kwargs):
-        self_copy = copy.deepcopy(self)
-        self_copy.transform = transform
-        self_copy.load_label = False
-        loader = lambda k: self_copy.__getitem__(k)
-        rotate = kwargs.pop('rotate', False)
-        return datasets.DatasetFactory(self.root, self.metadata).plot_grid(rotate=rotate, loader=loader, **kwargs)
-
-    def plot_predictions(self, y_true, y_pred, **kwargs):
-        from collections.abc import Iterable
-
-        if not isinstance(y_true, Iterable) and np.array(y_pred).ndim == 1:
-            y_true = [y_true]
-            y_pred = [y_pred]
-        if len(y_true) > 1:
-            header_cols = ["Query", ""] + [f"Match {i+1}" for i in range(len(y_pred[0]))]
-        else:
-            identity = self.metadata['identity'].to_numpy()
-            header_cols = [identity[y_true[0]], ""] + [identity[y_p] for y_p in y_pred[0]]
-        n_cols = len(header_cols)
-        idx = []
-        for y_t, y_p in zip(y_true, y_pred):
-            idx.append([y_t, -1] + list(y_p))
-        n_rows = kwargs.pop('n_rows', min(len(y_true), 5))
-        return self.plot_grid(idx=idx, n_rows=n_rows, n_cols=n_cols, header_cols=header_cols, **kwargs)
 
 def get_dataset(dataset_class, root_dataset):
     d = dataset_class(root_dataset)
