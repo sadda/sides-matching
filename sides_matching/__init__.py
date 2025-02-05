dfa46088882adecc1a208054c5afdf38a5214154
sadda <lukas.adam.cr@gmail.com>
2025-02-05 08:49:04 +0000

    Clean up

--- sides_matching/__init__.py
+++ sides_matching/__init__.py
@@ -1,6 +1,3 @@
-from .analysis import Analysis, Analysis_WildlifeDataset
-from .analysis import Analysis_HyenaID2022, Analysis_LeopardID2022, Analysis_NyalaData, Analysis_SeaTurtleID2022, Analysis_SeaTurtleIDHeads, Analysis_StripeSpotter, Analysis_WhaleSharkID, Analysis_ZindiTurtleRecall
-from .analysis import Analysis_AmvrakikosTurtles, Analysis_ReunionTurtles, Analysis_ZakynthosTurtles
 from .datasets import amvrakikos, reunion_green, reunion_hawksbill, zakynthos
 from .predictions import MegaDescriptor, TORSOOI, Aliked, Combined, Sift, Prediction
 from .utils import get_features, compute_predictions, visualise_matches, unpack
